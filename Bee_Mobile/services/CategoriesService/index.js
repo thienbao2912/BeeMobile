4db1edbe9f2a0cb64424af673fd3b155c0fb0220
Bao2912 <thienbaoct12@gmail.com>
2024-11-29 14:50:52 +0700

    fix API

--- Bee_Mobile/services/CategoriesService/index.js
+++ Bee_Mobile/services/CategoriesService/index.js
@@ -1,5 +1,5 @@
 import * as SecureStore from 'expo-secure-store';
-const API_URL = "http://172.16.30.124:4000/api";
+const API_URL = "http://10.0.2.2:4000/api";
 
 export const fetchAllCategories = async () => {
   try {
