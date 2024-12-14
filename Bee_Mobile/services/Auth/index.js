fab65932d0a108576958b0c0edf6959b965c96c1
tranhuudat050803 <132972331+tranhuudat050803@users.noreply.github.com>
2024-12-14 15:01:12 +0700

    API_URL

--- Bee_Mobile/services/Auth/index.js
+++ Bee_Mobile/services/Auth/index.js
@@ -1,6 +1,6 @@
 import request from "../../config/API/index";
 import * as SecureStore from 'expo-secure-store';
-const API_URL = 'http://192.168.1.7:4000/api';
+const API_URL = 'http://10.0.2.2:4000/api';
 const saveToken = async (key, value) => {
     try {
         await SecureStore.setItemAsync(key, value);
