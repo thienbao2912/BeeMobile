0f06a58d284a8e7da1a8b017c2b899be469b2428
PhamTheHien3333 <phamthehienapple@gmail.com>
2024-12-25 13:35:34 +0700

    push notification

--- Bee_Mobile/App.js
+++ Bee_Mobile/App.js
@@ -3,8 +3,11 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './navigation/AuthStack';
 import FlashMessage from "react-native-flash-message";
+import registerNNPushToken from 'native-notify';
 
  function App() {
+  registerNNPushToken(25582, 'DowBN7qkm455dOdSTMH4w0');
+  
         return (
           <>
           <NavigationContainer>
